6a4e9e27b516a2d4723be2d9756d2fe24b28fb1b
Mihai <mihail_danut24@yahoo.com>
2025-04-23 14:22:34 +0300

    added extraction methods

--- main.py
+++ main.py
@@ -8,7 +8,11 @@ if __name__ == "__main__":
     wm = cv2.imread("watermarks/flower.png", cv2.IMREAD_GRAYSCALE)
     _, wm = cv2.threshold(wm, 127, 255, cv2.THRESH_BINARY)
 
-    c = e.EFFTBlindDCC.embed(host=host, wm=wm, embedding_strength=11.01, secret_key=0, fftshit=False)
+    c = e.EFFTBlindDCC.embed(host=host, wm=wm, embedding_strength=11, secret_key=33, fftshit=False)
 
-    cc = e.EFFTBlindDCC.test_watermark(c, wm, secret_key=0, fftshit=False)
+    extracted_wm = e.EFFTBlindDCC.extract_watermark(c, target_shape=wm.shape, secret_key=33, fftshift=False)
+
+    cc = e.BaseWatermarker._correlation_coefficient(wm, extracted_wm)
     print(cc)
+    plt.imshow(extracted_wm)
+    plt.show()
